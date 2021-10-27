b0f177eaa6bc9576976c3a4b33c26db61b6da4b3
leandrotd <leandrotulian45@gmail.com>
2021-10-27 21:34:07 +0200

    Agregados comentarios al frontcontroller

--- app/frontcontroller.php
+++ app/frontcontroller.php
@@ -1,10 +1,15 @@
 <?php
 
 if (!isset($_REQUEST['p'])) {
+    //Llamada a la página principal
     call_user_func(array(new ProductoController(), 'index'));
 } else {
+    //Obtenemos el controlador a utilizar
     $controlador = new (ucwords(strtolower($_REQUEST['p'])) . 'Controller');
+    
+    //Recogemos la accion a realizar
     $accion = isset($_REQUEST['a']) ? strtolower($_REQUEST['a']) : 'index';
 
+    //Ejecutamos
     call_user_func(array($controlador, $accion));
 }
