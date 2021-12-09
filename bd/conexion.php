8aab1444723417894078d0b39a7d094f9434e03a
leandrotd <leandrotulian45@gmail.com>
2021-12-09 05:03:58 +0100

    Mejorar la lectura de los datos de conexion
    Agregado el csv con los datos

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,13 +1,13 @@
 <?php
 
-//Parametros de conexion a la base de datos
-define('HOST', 'localhost');
-define('NOMBBDD', 'supermercado');
-define('USUARIO', 'root');
-define('CLAVE', '');
-
 class Conexion
 {
+
+    private static $host;
+    private static $nomBBDD;
+    private static $usuario;
+    private static $clave;
+
     /**
      * Conexion con la base de datos
      *
@@ -15,8 +15,30 @@ class Conexion
      */
     public static function connect()
     {
-        $pdo = new PDO("mysql:host=" . HOST . ";dbname=" . NOMBBDD . ";charset=utf8", USUARIO, CLAVE);
+        //Lee el csv con los datos de conexion
+        Conexion::leerCSV();
+
+        //Conecta a la base de datos
+        $pdo = new PDO("mysql:host=" . Conexion::$host . ";dbname=" . Conexion::$nomBBDD . ";charset=utf8", Conexion::$usuario, Conexion::$clave);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
+    }
+
+    public static function leerCSV()
+    {
+        //Si no existe o no se puede leer el fichero, que termine de ejecutarse la aplicación
+        if (!file_exists("bd/datosConexion.csv") || !is_readable("bd/datosConexion.csv")) {
+            exit();
+        }
+        
+        $file = fopen('bd/datosConexion.csv', 'r');
+
+        //Leer la primera linea
+        if (($data = fgetcsv($file)) != false) {
+            Conexion::$host = $data[0];
+            Conexion::$nomBBDD = $data[1];
+            Conexion::$usuario = $data[2];
+            Conexion::$clave = $data[3];
+        }
     }
 }
